166eb1152f137b7de4f87d32b68376da1cd9489b
William Lempinen <wlempin@gmail.com>
2023-01-25 16:19:00 +0200

    current day params are now displayed on the screen

--- src/App.js
+++ src/App.js
@@ -1,45 +1,113 @@
 import Citybar from "./Citybar";
 import { useEffect, useState } from "react";
-import axios from "axios";
+
+//Helsingin koordinaatit
+const HEL_NO = 60.17;
+const HEL_EA = 24.95;
+//Turun koordinaatit
+const TU_NO = 60.45;
+const TU_EA = 22.28;
+//Tampereen koordinaatit
+const TA_NO = 61.50;
+const TA_EA = 23.80;
+
+
 
 function App() {
-  
   const [ city, setCity ] = useState("turku");
-  const [ data, setData ] = useState({});
-
-  //tämän päivän päivämäärään lisätään tunnit -> löydetään indeksi säälle
+  const [ tempData, setTempData ] = useState([]);
+  const [ north, setNorth ] = useState(TU_NO);
+  const [ east, setEast ] = useState(TU_EA);
+  const [ rain, setRain ] = useState([]);
+  const [ snow, setSnow ] = useState([]);
+  const [ cloud, setCloud ] = useState([]);
+  const [ wind, setWind ] = useState([]);
 
   let timeNow = new Date();
-  let day = timeNow.getDate();
-  let hours = timeNow.getHours();
+  //let day = timeNow.getDate();
+  let hour = timeNow.getHours();
+
+  //console.log(day);
+  console.log("kellonaika", hour);
 
 
-  console.log(timeNow);
-  console.log(day);
-  console.log(hours);
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=60.45&longitude=22.27&hourly=temperature_2m`;
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${north}&longitude=${east}&hourly=temperature_2m,rain,snowfall,cloudcover_mid,windspeed_120m&current_weather=true`;
 
   useEffect(() => {
-    axios
-    .get(url)
-    .then(res => {
-      console.log(res.data.hourly.temperature_2m[10]);
-      setData(res.data);
-    });
-  }, [city]);
-  console.log(data);
+    console.log("effect");
+    async function getData() {
+      const response = await fetch(url);
+      const data = await response.json();
+      setTempData(data.hourly.temperature_2m);
+      setRain(data.hourly.rain);
+      setSnow(data.hourly.snowfall);
+      setCloud(data.hourly.cloudcover_mid);
+      setWind(data.hourly.windspeed_120m);
+
+      console.log(data);
+    }
+
+    console.log("seuraavaksi kutsutaan getdata");
+    getData();
+
+  }, [city, north, east]);
+
+ 
+  //console.log("render city", city, "render timeNtemp", data);
+  const temperature = tempData[hour]
+  console.log("lämpötila", temperature);
+
+  const sateenMaara = rain[hour];
+  console.log("sadetta", sateenMaara);
   
+  const lumenMaara = snow[hour];
+  console.log("lunta", lumenMaara);
+
+  const pilvienMaara = cloud[hour];
+  console.log("pilviä", pilvienMaara);
+
+  const tuulenMaara = wind[hour];
+  console.log("tuulta", tuulenMaara);
+
+
+
+
+
+
 
   const changeCity = (e) => {
     e.preventDefault();
     setCity(e.target.value);
+    if (city === "turku") {
+      setNorth(TU_NO);
+      setEast(TU_EA);
+    } else if (city === "tampere") {
+      setNorth(TA_NO);
+      setEast(TA_EA);
+    } else {
+      setNorth(HEL_NO);
+      setEast(HEL_EA);
+    }
+    console.log(north, east);
     console.log(city);
   }
+
+
+
+
   
   return (
     <div className="App">
       <h1>Weather App</h1>
       <Citybar setCity={ changeCity }/>
+      <div className="content">
+        <h3>{ city.toLocaleUpperCase() }</h3>
+        <p>{ tempData[hour] }</p>
+        <p>{ rain[hour] }</p>
+        <p>{ snow[hour] }</p>
+        <p>{ cloud[hour] }</p>
+        <p>{ wind[hour] }</p>
+      </div>
     </div>
   );
 }
--- src/Citybar.js
+++ src/Citybar.js
@@ -1,17 +1,14 @@
-import React from "react";
-
 const Citybar = (props) => {
-    
     return (
         <div>
             <h2>Select city</h2>
-            <button onClick={ props.setCity } value={ "tampere" }>Tampere</button>
-            <button onClick={ props.setCity } value={ "helsinki" }>Helsinki</button>
-            <button onClick={ props.setCity } value={ "turku" }>Turku</button>
+            <div className="buttons">
+                <button onClick={ props.setCity } value={ "tampere" }>Tampere</button>
+                <button onClick={ props.setCity } value={ "helsinki" }>Helsinki</button>
+                <button onClick={ props.setCity } value={ "turku" }>Turku</button>
+            </div>
         </div>
-
     );
 }
-
 
 export default Citybar;
